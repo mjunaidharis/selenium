498c8c70f8a24437ea6315976c167146103b54b3
Junaid <mjunaidharis>
2018-12-24 23:28:40 -0800

    [Added more features in Login class]

--- Login.java
+++ Login.java
@@ -15,21 +15,28 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class Login {
 	WebDriver driver = null;
-	
+
 	Login(WebDriver driver) throws InterruptedException {
 		this.driver = driver;
+		goToSignInPage();
+	}
+
+	void goToSignInPage() throws InterruptedException {
+		if (driver.getTitle().equals("Sign in"))
+			return;
 		Thread.sleep(2000);
 		driver.findElement(By.cssSelector("#signin1")).click();
 		Thread.sleep(2000);
 	}
-	
-	void loginuser(String name, String pass) throws InterruptedException {	
+
+	void loginuser(String name, String pass) throws InterruptedException {
+		goToSignInPage();
 		driver.findElement(By.cssSelector("#email")).sendKeys(name);
 		driver.findElement(By.cssSelector("#pass")).sendKeys(pass);
 		driver.findElement(By.cssSelector("#signin")).click();
 		Thread.sleep(3000);
 	}
-	
-	public static void main(String []args) throws InterruptedException {
+
+	public static void main(String[] args) throws InterruptedException {
 	}
 }
